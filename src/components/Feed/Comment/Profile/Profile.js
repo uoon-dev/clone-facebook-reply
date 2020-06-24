bc6cb4d0434986a1da612a2f0a58f0e2f2e3a0cf
mumrik <uoon.dev@gmail.com>
2020-06-24 23:46:47 +0900

    fix profile style

--- src/components/Feed/Comment/Profile/Profile.js
+++ src/components/Feed/Comment/Profile/Profile.js
@@ -1,11 +1,13 @@
-import React from 'react';
+
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
 import * as styles from './profileStyle';
 
 const profileImageUrl = 'https://picsum.photos/seed/picsum/32/32';
 
 const profile = props => (
   <div>
-    <img src={profileImageUrl} css={styles.profleImage} alt="profile"></img>
+    <img src={profileImageUrl} css={styles.proflieImage} alt="profile"></img>
   </div>
 )
 
--- src/components/Feed/Comment/Profile/profileStyle.js
+++ src/components/Feed/Comment/Profile/profileStyle.js
@@ -1,6 +1,7 @@
 import { css } from '@emotion/core';
 
-export const profleImage = css`
+export const proflieImage = css`
   width: 32px;
   height: 32px;
+  border-radius: 50%;
 `;
