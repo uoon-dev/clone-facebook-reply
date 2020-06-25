54e4db59a757f918bc9a2bb042f74ed4502319c4
mumrik <uoon.dev@gmail.com>
2020-06-25 11:48:09 +0900

    create comment input component

---
+++ src/components/Feed/Comment/CommentInput/CommentInput.js
@@ -0,0 +1,25 @@
+
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+// import { useForm } from 'react-hook-form';
+import TextareaAutosize from 'react-textarea-autosize';
+
+import * as text from 'constants/text';
+// import * as styles from './commentInputStyle';
+
+
+const commentInput = props => {
+  return (
+    <div className="CommentForm">
+      <TextareaAutosize 
+        defaultValue={text.comment.reply}
+        onKeyDown={(e) => e.key === 'Enter' ? props.createCommentContent() : null}
+        minRows={1}
+        value={props.content}
+        name="commentContent"
+      />
+    </div>
+  )
+}
+
+export default commentInput;
