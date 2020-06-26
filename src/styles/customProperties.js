b06987dc367659bf0e7eb808f3adfcb5d6daf498
mumrik <uoon.dev@gmail.com>
2020-06-26 11:50:13 +0900

    add reset layout, appearance, list style

--- src/styles/customProperties.js
+++ src/styles/customProperties.js
@@ -1,1 +1,17 @@
 import { css } from '@emotion/core';
+
+export const resetLayout = css`
+  margin: 0;
+  padding: 0;
+`;
+
+export const resetAppearance = css`
+  border: 0;
+  appearance: none;
+`;
+
+export const resetList = css`
+  ${resetLayout}
+  ${resetAppearance}
+  list-style: none;
+`;
